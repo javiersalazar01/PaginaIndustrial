44442ab1717c441cd6361e996f0c29f4ddff3d2e
karlagutz <karlagutz07@gmail.com>
2015-09-18 13:30:50 -0700

    logo uson

--- Plantillas/Plantilla a usar/index.php
+++ Plantillas/Plantilla a usar/index.php
@@ -47,11 +47,12 @@
 		                    <span class="icon-bar"></span>
 		                </button>
 		                <a class="navbar-brand" href="index.html">
-		                	<img class="img-responsive" src="images/logo.png" alt="logo">
+		                	<img class="img-responsive escudo" src="images/EscudoUson.png" alt="logo">
 		                </a>                    
 		            </div>
 		            <div class="collapse navbar-collapse">
-		                <ul class="nav navbar-nav navbar-right">                 
+		                <ul class="nav navbar-nav navbar-right">    
+		                 <li ><img class="logo-industrial img-responsive" src="images/logo.png"></li>             
 		                    <li ><a href="./index.php" >Inicio</a></li>
 		                    <li class="dropdown">
           						<a class="dropdown-toggle" data-toggle="dropdown" >Departamento

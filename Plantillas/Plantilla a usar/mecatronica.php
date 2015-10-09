8b08e01a7f79c5acf3aae5025cf6fbaf645e8ca5
karlagutz <karlagutz07@gmail.com>
2015-10-09 09:14:05 -0700

    Mapa de Mecatronica

--- Plantillas/Plantilla a usar/mecatronica.php
+++ Plantillas/Plantilla a usar/mecatronica.php
@@ -86,61 +86,61 @@ $(document).ready(function(e) {
 	<h2>Mapa curricular</h2>
 		<img src="images/mapasCurriculares/mapa_ime.png"  usemap="#Map" class="img-responsive">
 <map name="Map" id="Map">
-<area shape="rect" coords="252,540,544,708" href="http://dii.isi.uson.mx/asignaturas/6880.pdf" target="_blank" title="Calculo Diferencial e Integral I" />
-<area shape="rect" coords="248,804,552,964" href="http://dii.isi.uson.mx/asignaturas/6881.pdf" target="_blank" title="Algebra"/>
+<area shape="rect" coords="252,540,544,708" href="http://dii.isi.uson.mx/asignaturas/6880.pdf" target="_blank" alt="Calculo Diferencial e Integral I" title="Calculo Diferencial e Integral I" />
+<area shape="rect" coords="248,804,552,964" href="http://dii.isi.uson.mx/asignaturas/6881.pdf" target="_blank" alt="Álgebra" title="Álgebra"/>
 <area shape="rect" coords="240,1066,556,1250" href="#" />
-<area shape="rect" coords="244,1336,552,1516" href="#" target="_blank" alt="quimica" />
-<area shape="rect" coords="247,1613,550,1791" href="#" target="_blank" alt="ntic" />
-<area shape="rect" coords="247,1888,552,2058" href="#" target="_blank" alt="aprender a aprender" />
-<area shape="rect" coords="250,2150,552,2327" href="#" target="_blank" alt="Introduccion a  la ime" />
-<area shape="rect" coords="597,536,897,705" href="#" target="_blank" alt="Calculo2" />
-<area shape="rect" coords="605,802,905,980" href="#" target="_blank" alt="Prob y Est" />
-<area shape="rect" coords="591,1077,900,1247" href="#" target="_blank" alt="Fisica 1 con lab" />
-<area shape="rect" coords="605,1344,902,1525" href="#" target="_blank" alt="ciencia de los materiales" />
-<area shape="rect" coords="605,1611,902,1791" href="#" target="_blank" alt="Dibujo Inds" />
-<area shape="rect" coords="602,1875,902,2055" href="#" target="_blank" alt="Top de mate discretas" />
-<area shape="rect" coords="947,2150,1255,2327" href="#" target="_blank" alt="Sustentabilidad" />
-<area shape="rect" coords="955,1886,1261,2066" href="#" target="_blank" alt="caracteristicas" />
-<area shape="rect" coords="952,1605,1263,1772" href="#" target="_blank" alt="dibujo industrial 2" />
-<area shape="rect" coords="955,1336,1261,1513" href="#" target="_blank" alt=" Metrologia y Norm" />
-<area shape="rect" coords="966,1072,1258,1238" href="#" target="_blank" alt="Mecanica Vectorial 1" />
-<area shape="rect" coords="952,791,1252,961" href="#" target="_blank" alt="Fluidos y calor" />
-<area shape="rect" coords="1305,794,1602,977" href="#" target="_blank" alt="Electromagnetismo" />
-<area shape="rect" coords="1305,1066,1605,1250" href="#" target="_blank" alt="Mecanica Vectorial 2" />
-<area shape="rect" coords="1305,1344,1608,1519" href="#" target="_blank" alt="Resistencia de materiales" />
-<area shape="rect" coords="1302,1605,1608,1780" href="#" target="_blank" alt="Progra 1" />
-<area shape="rect" coords="1302,1888,1611,2061" href="#" target="_blank" alt="Costos" />
-<area shape="rect" coords="1650,1883,1955,2058" href="#" target="_blank" alt="Circuitos Hidraulicos y neumaticos" />
-<area shape="rect" coords="1661,1611,1966,1789" href="#" target="_blank" alt="Progra 2" />
-<area shape="rect" coords="1658,1341,1952,1522" href="#" target="_blank" alt="Diseño de elementos Mecanicos" />
-<area shape="rect" coords="1650,1072,1958,1250" href="#" target="_blank" alt="Diseño Cinematico de elementos de Maquinas" />
-<area shape="rect" coords="1655,791,1958,980" href="#" target="_blank" alt="Circuitos Electricos" />
-<area shape="rect" coords="952,536,1261,711" href="#" target="_blank" alt="Calculo3" />
-<area shape="rect" coords="1311,530,1613,705" href="#" target="_blank" alt="Ecuaciones" />
-<area shape="rect" coords="1658,538,1958,705" href="#" target="_blank" alt="Dinamica de Sis" />
-<area shape="rect" coords="2005,533,2311,694" href="#" target="_blank" alt="Maquinas Electricas" />
-<area shape="rect" coords="2005,794,2311,969" href="#" target="_blank" alt="Electronica analogica" />
-<area shape="rect" coords="2008,1072,2308,1244" href="#" target="_blank" alt="Proceso de manofactura 1" />
-<area shape="rect" coords="2019,1350,2316,1525" href="#" target="_blank" alt="Admon de proyectos" />
-<area shape="rect" coords="2008,1883,2311,2058" href="#" target="_blank" alt="Maquinas Hidraulicas" />
-<area shape="rect" coords="2002,2155,2308,2333" href="#" target="_blank" alt="Analisis numero y programacion" />
-<area shape="rect" coords="2722,2158,3019,2347" href="#" target="_blank" alt="Etica" />
-<area shape="rect" coords="2355,1886,2663,2058" href="#" target="_blank" alt="Redes de datos y aplicaciones en inet" />
-<area shape="rect" coords="2363,1341,2663,1522" href="#" target="_blank" alt="Gestion demantenimento" />
-<area shape="rect" coords="2358,1077,2661,1244" href="#" target="_blank" alt="Electronica industrial" />
-<area shape="rect" coords="2361,805,2663,983" href="#" target="_blank" alt="Electronica Digital" />
-<area shape="rect" coords="2363,536,2669,705" href="#" target="_blank" alt="Ingenieria de Control 1" />
-<area shape="rect" coords="2711,544,3016,711" href="#" target="_blank" alt="Controll de maquinas electricas" />
-<area shape="rect" coords="2716,1066,3019,1244" href="#" target="_blank" alt="Gestion de Recurso Humano" />
-<area shape="rect" coords="2716,1605,3019,1780" href="#" target="_blank" alt="Instrumentacion Virtual" />
-<area shape="rect" coords="2716,1883,3013,2086" href="#" target="_blank" alt="Microcontroladores" />
-<area shape="rect" coords="3061,1894,3369,2066" href="#" target="_blank" alt="Controladores Logicos programables" />
-<area shape="rect" coords="3058,1613,3363,1786" href="#" target="_blank" alt="Manofactura avanzada" />
-<area shape="rect" coords="3063,1338,3358,1516" href="#" target="_blank" alt="Admon Estratigica" />
-<area shape="rect" coords="3058,1072,3363,1244" href="#" target="_blank" alt="Cultura emprendedora" />
-<area shape="rect" coords="3413,1066,3716,1252" href="#" alt="Estancia Profesional" />
-<area shape="rect" coords="3413,1341,3711,1544" href="#" alt="Actividades culturales" />
-<area shape="rect" coords="2352,1327,2652,1516" href="#" target="_blank" alt="Gestion de mantenimiento" />
+<area shape="rect" coords="244,1336,552,1516" href="http://dii.isi.uson.mx/asignaturas/6883.pdf" target="_blank" alt="Química I"  title="Química I" />
+<area shape="rect" coords="247,1613,550,1791" href="http://dii.isi.uson.mx/asignaturas/0123.pdf" target="_blank" alt="Nuevas Tecnologías de la Información la Comunicación" title="Nuevas Tecnologías de la Información la Comunicación" />
+<area shape="rect" coords="247,1888,552,2058" href="http://dii.isi.uson.mx/asignaturas/0120.pdf" target="_blank" alt="Estrategias para Aprender a Aprender" title="Estrategias para Aprender a Aprender" />
+<area shape="rect" coords="250,2150,552,2327" href="http://dii.isi.uson.mx/asignaturas/9930.pdf" target="_blank" alt="Introducción a la Mecatrónica" title="Introducción a la Mecatrónica" />
+<area shape="rect" coords="597,536,897,705" href="http://dii.isi.uson.mx/asignaturas/6884.pdf" target="_blank" alt="Cálculo Diferencial e Integral II" title="Cálculo Diferencial e Integral II" />
+<area shape="rect" coords="605,802,905,980" href="http://dii.isi.uson.mx/asignaturas/6890.pdf" target="_blank" alt="Probalilidad y Estadística" title="Probalilidad y Estadística" />
+<area shape="rect" coords="591,1077,900,1247" href="http://dii.isi.uson.mx/asignaturas/6885.pdf" target="_blank" alt="Física I con Laboratorio" title="Física I con Laboratorio" />
+<area shape="rect" coords="605,1344,902,1525" href="http://dii.isi.uson.mx/asignaturas/9932.pdf" target="_blank" alt="Ciencia de los Materiales" title="Ciencia de los Materiales" />
+<area shape="rect" coords="605,1611,902,1791" href="http://dii.isi.uson.mx/asignaturas/9931.pdf" target="_blank" alt="Dibujo Industrial I" title="Dibujo Industrial I" />
+<area shape="rect" coords="602,1875,902,2055" href="http://dii.isi.uson.mx/asignaturas/9938.pdf" target="_blank" alt="Tópicos de Matemáticas Discretas" title="Tópicos de Matemáticas Discretas" />
+<area shape="rect" coords="947,2150,1255,2327" href="http://dii.isi.uson.mx/asignaturas/6893.pdf" target="_blank" alt="Sustentabilidad en las Ingenierías" title="Sustentabilidad en las Ingenierías" />
+<area shape="rect" coords="955,1886,1261,2066" href="http://dii.isi.uson.mx/asignaturas/0121.pdf" target="_blank" alt="Características de la Sociedad Actual" title="Características de la Sociedad Actual" />
+<area shape="rect" coords="952,1605,1263,1772" href="http://dii.isi.uson.mx/asignaturas/9935.pdf" target="_blank" alt="Dibujo Industrial II" title="Dibujo Industrial II" />
+<area shape="rect" coords="955,1336,1261,1513" href="#" target="_blank" alt=" Metrología y Normalización" title =" Metrología y Normalización" />
+<area shape="rect" coords="966,1072,1258,1238" href="http://dii.isi.uson.mx/asignaturas/9936.pdf" target="_blank" alt="Mecánica Vectorial Para Ingenieros I" title="Mecánica Vectorial Para Ingenieros I" />
+<area shape="rect" coords="952,791,1252,961" href="#" target="_blank" alt="Fluidos y Calor" title="Fluidos y Calor" />
+<area shape="rect" coords="1305,794,1602,977" href="#" target="_blank" alt="Electromagnetismo" title="Electromagnetismo" />
+<area shape="rect" coords="1305,1066,1605,1250" href="http://dii.isi.uson.mx/asignaturas/9940.pdf" target="_blank" alt="Mecánica Vectorial Para Ingenieros II" title="Mecánica Vectorial Para Ingenieros II" />
+<area shape="rect" coords="1305,1344,1608,1519" href="http://dii.isi.uson.mx/asignaturas/9937.pdf" target="_blank" alt="Resistencia de Materiales" title="Resistencia de Materiales" />
+<area shape="rect" coords="1302,1605,1608,1780" href="http://dii.isi.uson.mx/asignaturas/7973m.pdf" target="_blank" alt="Programación Para Ingenieros I" title="Programación Para Ingenieros I" />
+<area shape="rect" coords="1302,1888,1611,2061" href="http://dii.isi.uson.mx/asignaturas/7977.pdf" target="_blank" alt="Costos en Ingeniería" title="Costos en Ingeniería" />
+<area shape="rect" coords="1650,1883,1955,2058" href="http://dii.isi.uson.mx/asignaturas/9944.pdf" target="_blank" alt="Circuitos Hidráulicos y Neumáticos" title="Circuitos Hidráulicos y Neumáticos" />
+<area shape="rect" coords="1661,1611,1966,1789" href="http://dii.isi.uson.mx/asignaturas/7971m.pdf" target="_blank" alt="Programación Para Ingenieros II" title="Programación Para Ingenieros II" />
+<area shape="rect" coords="1658,1341,1952,1522" href="http://dii.isi.uson.mx/asignaturas/9942.pdf" target="_blank" alt="Diseño de Elementos Mecánicos" title="Diseño de Elementos Mecánicos" />
+<area shape="rect" coords="1650,1072,1958,1250" href="http://dii.isi.uson.mx/asignaturas/9943.pdf" target="_blank" alt="Diseño Cinemático de Elementos de Máquinas" title="Diseño Cinemático de Elementos de Máquinas" />
+<area shape="rect" coords="1655,791,1958,980" href="http://dii.isi.uson.mx/asignaturas/7978.pdf" target="_blank" alt="Circuitos Eléctricos" title="Circuitos Eléctricos" />
+<area shape="rect" coords="952,536,1261,711" href="http://dii.isi.uson.mx/asignaturas/6889.pdf" target="_blank" alt="Cálculo Diferencial e Integral III" title="Cálculo Diferencial e Integral III" />
+<area shape="rect" coords="1311,530,1613,705" href="http://dii.isi.uson.mx/asignaturas/6895.pdf" target="_blank" alt="Ecuaciones Diferenciales I" title="Ecuaciones Diferenciales I" />
+<area shape="rect" coords="1658,538,1958,705" href="#" target="_blank" alt="Dinámica de Sistemas" title="Dinamica de Sistemas" />
+<area shape="rect" coords="2005,533,2311,694" href="http://dii.isi.uson.mx/asignaturas/9945.pdf" target="_blank" alt="Máquinas Eléctricas" title="Máquinas Eléctricas" />
+<area shape="rect" coords="2005,794,2311,969" href="http://dii.isi.uson.mx/asignaturas/9946.pdf" target="_blank" alt="Electrónica Analógica" title="Electrónica Analógica" />
+<area shape="rect" coords="2008,1072,2308,1244" href="http://dii.isi.uson.mx/asignaturas/7981.pdf" target="_blank" alt="Proceso de Manufactura I" title="Proceso de Manufactura I" />
+<area shape="rect" coords="2019,1350,2316,1525" href="http://dii.isi.uson.mx/asignaturas/9948.pdf" target="_blank" alt="Administración de Proyectos" title="Administración de Proyectos" />
+<area shape="rect" coords="2008,1883,2311,2058" href="http://dii.isi.uson.mx/asignaturas/9947.pdf" target="_blank" alt="Máquinas Hidráulicas" title="Máquinas Hidráulicas" />
+<area shape="rect" coords="2002,2155,2308,2333" href="http://dii.isi.uson.mx/asignaturas/9957.pdf" target="_blank" alt="Análisis Numérico y Programación" title="Análisis Numérico y Programación" />
+<area shape="rect" coords="2722,2158,3019,2347" href="http://dii.isi.uson.mx/asignaturas/0124.pdf" target="_blank" alt="Ética y Desarrollo Profesional" title="Ética y Desarrollo Profesional" />
+<area shape="rect" coords="2355,1886,2663,2058" href="http://dii.isi.uson.mx/asignaturas/9950.pdf" target="_blank" alt="Redes de Datos y Aplicaciones en Internet" title="Redes de Datos y Aplicaciones en Internet" />
+<area shape="rect" coords="2363,1341,2663,1522" href="http://dii.isi.uson.mx/asignaturas/7994.pdf" target="_blank" alt="Gestión de Mantenimiento" title="Gestión de Mantenimiento" />
+<area shape="rect" coords="2358,1077,2661,1244" href="http://dii.isi.uson.mx/asignaturas/9951.pdf" target="_blank" alt="Electrónica Industrial" title="Electrónica Industrial" />
+<area shape="rect" coords="2361,805,2663,983" href="http://dii.isi.uson.mx/asignaturas/9949.pdf" target="_blank" alt="Electrónica Digital" title="Electrónica Digital" />
+<area shape="rect" coords="2363,536,2669,705" href="http://dii.isi.uson.mx/asignaturas/9958.pdf" target="_blank" alt="Ingeniería de Control I" title="Ingeniería de Control I" />
+<area shape="rect" coords="2711,544,3016,711" href="http://dii.isi.uson.mx/asignaturas/9952.pdf" target="_blank" alt="Control de Máquinas Eléctricas" title="Control de Máquinas Eléctricas" />
+<area shape="rect" coords="2716,1066,3019,1244" href="http://dii.isi.uson.mx/asignaturas/7995.pdf" target="_blank" alt="Gestión del Recurso Humano" title="Gestión del Recurso Humano" />
+<area shape="rect" coords="2716,1605,3019,1780" href="http://dii.isi.uson.mx/asignaturas/9954.pdf" target="_blank" alt="Instrumentación Virtual" title="Instrumentación Virtual" />
+<area shape="rect" coords="2716,1883,3013,2086" href="http://dii.isi.uson.mx/asignaturas/9953.pdf" target="_blank" alt="Microcontroladores" title="Microcontroladores" />
+<area shape="rect" coords="3061,1894,3369,2066" href="http://dii.isi.uson.mx/asignaturas/9955.pdf" target="_blank" alt="Controladores Lógicos Programables" title="Controladores Lógicos Programables" />
+<area shape="rect" coords="3058,1613,3363,1786" href="http://dii.isi.uson.mx/asignaturas/9956.pdf" target="_blank" alt="Manufactura Avanzada" title="Manufactura Avanzada" />
+<area shape="rect" coords="3063,1338,3358,1516" href="http://dii.isi.uson.mx/asignaturas/7976.pdf" target="_blank" alt="Administración Estratégica" title="Administración Estratégica" />
+<area shape="rect" coords="3058,1072,3363,1244" href="http://dii.isi.uson.mx/asignaturas/6906.pdf" target="_blank" alt="Cultura Emprendedora" title="Cultura Emprendedora" />
+<area shape="rect" coords="3413,1066,3716,1252" href="#" alt="Estancia Profesional" title="Estancia Profesional" />
+<area shape="rect" coords="3413,1341,3711,1544" href="#" alt="Actividades culturales" title="Actividades culturales"/>
+<area shape="rect" coords="2352,1327,2652,1516" href="http://dii.isi.uson.mx/asignaturas/7994.pdf" target="_blank" alt="Gestión de Mantenimiento" title="Gestión de Mantenimiento" />
 </map>
 </div>
 
